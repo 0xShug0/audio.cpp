6ab0aed85988cfe6b8434576e056677fef19c764
0xShug0 <231717474+0xShug0@users.noreply.github.com>
2026-08-11 19:51:08 -0400

    Fix HIP build without Sage Attention 2

--- external/ggml/src/ggml-cuda/sage-attn2.cuh
+++ external/ggml/src/ggml-cuda/sage-attn2.cuh
@@ -1,6 +1,26 @@
+#pragma once
+
 #include "common.cuh"
 
-#ifdef GGML_CUDA_SAGE_ATTN2_ENABLED
+#if defined(GGML_USE_HIP)
+
+static inline void ggml_cuda_sage_attn2(ggml_backend_cuda_context &, ggml_tensor *) {
+    GGML_ABORT("SAGE_ATTN2 is not supported by the HIP backend");
+}
+
+static inline void ggml_cuda_sage_attn2_i8(ggml_backend_cuda_context &, ggml_tensor *) {
+    GGML_ABORT("SAGE_ATTN2_I8 is not supported by the HIP backend");
+}
+
+static inline bool ggml_cuda_sage_attn2_supported(int, const ggml_tensor *) {
+    return false;
+}
+
+static inline bool ggml_cuda_sage_attn2_i8_supported(int, const ggml_tensor *) {
+    return false;
+}
+
+#elif defined(GGML_CUDA_SAGE_ATTN2_ENABLED)
 
 void ggml_cuda_sage_attn2(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
 
