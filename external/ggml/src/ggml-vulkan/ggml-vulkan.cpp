9033f306ce6c41097503a2db2c9ef19962f3aebb
Aditya Waghmare <adi2004waghmare@gmail.com>
2026-09-13 23:29:17 +0530

    vulkan: fix COL2IM_1D dispatch for large tensors

--- external/ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ external/ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -10381,8 +10381,7 @@ static void ggml_vk_op_f32(ggml_backend_vk_context * ctx, vk_context& subctx, co
         } break;
     case GGML_OP_COL2IM_1D:
         {
-            const uint32_t total = (uint32_t)(dst->ne[0] * dst->ne[1]);
-            elements = { total, 1, 1 };
+            elements = { uint32_t(dst->ne[0]), uint32_t(dst->ne[1]), 1 };
         } break;
     case GGML_OP_IM2COL_3D:
         {
